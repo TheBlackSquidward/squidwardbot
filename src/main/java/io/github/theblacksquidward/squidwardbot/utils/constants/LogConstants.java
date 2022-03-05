0c13f8e22b3132b567a58f2a50190443cab0cbff
TheBlackSquidward <SillySausages98>
2022-03-05 14:17:45 +0000

    Started work on some logging.

--- src/main/java/io/github/theblacksquidward/squidwardbot/utils/constants/LogConstants.java
+++ src/main/java/io/github/theblacksquidward/squidwardbot/utils/constants/LogConstants.java
@@ -1,7 +1,10 @@
 package io.github.theblacksquidward.squidwardbot.utils.constants;
 
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
 public class LogConstants {
 
-
+    public static final Marker AUDIO = MarkerFactory.getMarker("AUDIO");
 
 }

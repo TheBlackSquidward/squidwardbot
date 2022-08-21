1f64f958d64836df4ed10fc16b11a627de139715
Harry Greentree <harrygreentree@icloud.com>
2022-08-21 17:03:37 +0100

    The volume option can never be null as its required by the command.

--- squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/VolumeCommand.java
+++ squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/VolumeCommand.java
@@ -42,10 +42,6 @@ public class VolumeCommand extends AbstractAudioCommand {
         }
         if(event.getSubcommandName().equalsIgnoreCase("set")) {
             final OptionMapping volume = event.getOption("volume");
-            if(volume == null) {
-                event.replyEmbeds(createMusicReply("Please specify a volume.")).queue();
-                return;
-            }
             AudioManager.setVolume(guild, volume.getAsInt());
             event.replyEmbeds(createMusicReply("The volume has successfully been set to " + AudioManager.getVolume(guild) + "`.")).queue();
         }

86f149aac062b5eb07aaf36e1c745e266d6758ce
Harry Greentree <harrygreentree@icloud.com>
2022-08-27 14:34:20 +0100

    Changed default music reply embed.

--- squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/AbstractAudioCommand.java
+++ squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/AbstractAudioCommand.java
@@ -5,12 +5,15 @@ import io.github.theblacksquidward.squidwardbot.core.constants.ColorConstants;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import java.time.Instant;
+
 public abstract class AbstractAudioCommand extends SquidwardBotCommand {
 
     protected static MessageEmbed createMusicReply(String text) {
         return new EmbedBuilder()
+                .setTimestamp(Instant.now())
                 .setColor(ColorConstants.PRIMARY_COLOR)
-                .setAuthor("|  " + text, null, "https://avatars.githubusercontent.com/u/65785034?v=4")
+                .setDescription(text)
                 .build();
     }
 

566b33bf3f86c5d886efc4c962e4b3833c3d0e03
Harry Greentree <harrygreentree@icloud.com>
2022-09-03 18:51:18 +0100

    Moved common logic to AbstractAudioCommand.

--- squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/AbstractAudioCommand.java
+++ squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/AbstractAudioCommand.java
@@ -1,11 +1,16 @@
 package io.github.theblacksquidward.squidwardbot.audio.commands;
 
+import genius.SongSearch;
+import io.github.theblacksquidward.squidwardbot.audio.AudioManager;
 import io.github.theblacksquidward.squidwardbot.core.commands.SquidwardBotCommand;
 import io.github.theblacksquidward.squidwardbot.core.constants.ColorConstants;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import java.io.IOException;
 import java.time.Instant;
+import java.util.LinkedList;
 
 public abstract class AbstractAudioCommand extends SquidwardBotCommand {
 
@@ -15,6 +20,16 @@ public abstract class AbstractAudioCommand extends SquidwardBotCommand {
                 .setColor(ColorConstants.PRIMARY_COLOR)
                 .setDescription(text)
                 .build();
+    }
+
+    protected static SongSearch.Hit getHit(Guild guild) {
+        try {
+            final SongSearch songSearch = AudioManager.getLyrics(guild);
+            final LinkedList<SongSearch.Hit> hits = songSearch.getHits();
+            return hits.isEmpty() ? null : hits.getFirst();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

20ba87d180f52a7324c8bdb6b07144a4b5857680
Harry Greentree <harrygreentree@icloud.com>
2022-09-07 23:17:28 +0100

    Working implementation of the lyrics command.

--- squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/LyricsCommand.java
+++ squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/commands/LyricsCommand.java
@@ -9,18 +9,84 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.AudioChannel;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Command
 public class LyricsCommand extends AbstractAudioCommand{
 
     private static final Map<Long, String> ID_LYRIC_MAP = new HashMap<>();
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+        if(!event.isFromGuild()) return;
+        if(!event.getComponentId().startsWith("lyric")) return;
+        String componentId = event.getComponentId();
+        String[] componentIdArray = componentId.split("_");
+
+        final String buttonId = componentIdArray[0];
+        final long channelId = Long.parseLong(componentIdArray[1]);
+        final long userId = Long.parseLong(componentIdArray[2]);
+        final long hitId = Long.parseLong(componentIdArray[3]);
+        final int oldPage = Integer.parseInt(componentIdArray[4].replace("page", ""));
+        final String instruction = componentIdArray[5];
+
+        int newPage = oldPage;
+        if(instruction.equalsIgnoreCase("close")) {
+            event.deferEdit().flatMap(InteractionHook::deleteOriginal).queue();
+            ID_LYRIC_MAP.remove(hitId);
+        }
+        if(instruction.equalsIgnoreCase("next")) {
+            newPage++;
+            final String lyrics = ID_LYRIC_MAP.get(hitId);
+            final List<String> requestedLyrics = getLyricsPage(lyrics.lines().collect(Collectors.toList()), newPage);
+            if(requestedLyrics.isEmpty()) {
+                event.editButton(event.getButton().withDisabled(true)).queue();
+                return;
+            }
+            final String nextPage = String.join("\n", requestedLyrics);
+            final ActionRow actionRow = event.getMessage().getActionRows().get(0);
+            Button previous = actionRow.getButtons().get(0);
+            Button close = actionRow.getButtons().get(1);
+            Button next = actionRow.getButtons().get(2);
+            previous = previous.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_prev").withDisabled(false);
+            close = close.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_close").withDisabled(false);
+            next = next.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_next").withDisabled(false);
+            event.editMessageEmbeds(
+                    new EmbedBuilder(event.getMessage().getEmbeds().get(0)).setDescription(nextPage).build())
+                    .setComponents(ActionRow.of(previous, close, next)).queue();
+        }
+        if(instruction.equalsIgnoreCase("prev")) {
+            newPage--;
+            final String lyrics = ID_LYRIC_MAP.get(hitId);
+            final List<String> requestedLyrics = getLyricsPage(lyrics.lines().collect(Collectors.toList()), newPage);
+            if(requestedLyrics.isEmpty()) {
+                event.editButton(event.getButton().withDisabled(true)).queue();
+                return;
+            }
+            final String nextPage = String.join("\n", requestedLyrics);
+            final ActionRow actionRow = event.getMessage().getActionRows().get(0);
+            Button previous = actionRow.getButtons().get(0);
+            Button close = actionRow.getButtons().get(1);
+            Button next = actionRow.getButtons().get(2);
+            previous = previous.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_prev").withDisabled(false);
+            close = close.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_close").withDisabled(false);
+            next = next.withId(buttonId + "_" + channelId + "_" + userId + "_" + hitId + "_" + newPage + "_next").withDisabled(false);
+            event.editMessageEmbeds(
+                            new EmbedBuilder(event.getMessage().getEmbeds().get(0)).setDescription(nextPage).build())
+                    .setComponents(ActionRow.of(previous, close, next)).queue();
+        }
+    }
 
     @Override
     public void onSlashCommand(SlashCommandInteractionEvent event) {
@@ -50,7 +116,15 @@ public class LyricsCommand extends AbstractAudioCommand{
                 return;
             }
             final Hit hit = hits.getFirst();
-            event.replyEmbeds(getLyricsEmbed(hit)).queue();
+            final String lyrics = hit.fetchLyrics();
+            ID_LYRIC_MAP.put(hit.getId(), lyrics);
+            event.replyEmbeds(getLyricsEmbed(hit, lyrics))
+                    .setComponents(ActionRow.of(
+                            Button.primary("lyrics_" + event.getChannel().getId() + "_" + event.getUser().getId() + "_" + hit.getId() + "_page0" + "_prev", Emoji.fromUnicode("◀️"))
+                                    .asDisabled(),
+                            Button.danger("lyrics_" + event.getChannel().getId() + "_" + event.getUser().getId() + "_" + hit.getId() + "_page0" + "_close", Emoji.fromUnicode("⚠️")),
+                            Button.primary("lyrics_" + event.getChannel().getId() + "_" + event.getUser().getId() + "_" + hit.getId() + "_page0" + "_next", Emoji.fromUnicode("▶️"))
+                    )).queue();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -66,16 +140,32 @@ public class LyricsCommand extends AbstractAudioCommand{
         return "Returns the lyrics of the currently playing song.";
     }
 
-    private MessageEmbed getLyricsEmbed(Hit hit) {
+    private MessageEmbed getLyricsEmbed(Hit hit, String lyrics) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setThumbnail(hit.getThumbnailUrl());
         embedBuilder.setTimestamp(Instant.now());
         embedBuilder.setColor(ColorConstants.PRIMARY_COLOR);
-        embedBuilder.setFooter(hit.getArtist().getName(), hit.getArtist().getImageUrl());
-        embedBuilder.setTitle(hit.getTitleWithFeatured());
-        final String lyrics = hit.fetchLyrics();
-        embedBuilder.setDescription(lyrics.substring(0, Math.min(lyrics.length(), 1024)));
+        embedBuilder.setFooter(hit.getArtist().getName() + " • Page 1", hit.getArtist().getImageUrl());
+        embedBuilder.setTitle(hit.getTitleWithFeatured(), hit.getUrl());
+        embedBuilder.setDescription(String.join("\n", getLyricsPage(lyrics.lines().collect(Collectors.toList()), 0)));
         return embedBuilder.build();
+    }
+
+    private List<String> getLyricsPage(List<String> lines, int page) {
+        int minBound = 1024 * page;
+        int maxBound = 1024 * (1 + page);
+        int characterCounter = 0;
+        var output = new ArrayList<String>();
+        for (String line : lines) {
+            characterCounter += line.length();
+            if(maxBound < characterCounter) {
+                break;
+            }
+            if(minBound < characterCounter) {
+                output.add(line);
+            }
+        }
+        return output;
     }
 
 }

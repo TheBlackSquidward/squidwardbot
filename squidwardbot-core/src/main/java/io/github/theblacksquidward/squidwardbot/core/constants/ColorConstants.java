1016f28ceb3611a283181175d01f74df7afb811d
Harry Greentree <harrygreentree@icloud.com>
2022-09-15 17:03:17 +0100

    Added additional colors.

--- squidwardbot-core/src/main/java/io/github/theblacksquidward/squidwardbot/core/constants/ColorConstants.java
+++ squidwardbot-core/src/main/java/io/github/theblacksquidward/squidwardbot/core/constants/ColorConstants.java
@@ -5,5 +5,7 @@ import java.awt.*;
 public class ColorConstants {
 
     public static final Color PRIMARY_COLOR = new Color(5, 224, 250);
-
+    public static final Color SECONDARY_COLOR = new Color(250, 197, 5);
+    public static final Color TERTIARY_COLOR = new Color(250, 5, 115);
+;
 }

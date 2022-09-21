6b8de928d213a953002a7ce53053bebf3df08064
Harry Greentree <harrygreentree@icloud.com>
2022-09-21 01:26:53 +0100

    Removed random and unnecessary semicolon.

--- squidwardbot-core/src/main/java/io/github/theblacksquidward/squidwardbot/core/constants/ColorConstants.java
+++ squidwardbot-core/src/main/java/io/github/theblacksquidward/squidwardbot/core/constants/ColorConstants.java
@@ -7,5 +7,5 @@ public class ColorConstants {
     public static final Color PRIMARY_COLOR = new Color(5, 224, 250);
     public static final Color SECONDARY_COLOR = new Color(250, 197, 5);
     public static final Color TERTIARY_COLOR = new Color(250, 5, 115);
-;
+
 }

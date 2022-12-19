4eb2bad999411311b8820d743c405a928fdc1e54
Harry Greentree <harrygreentree@icloud.com>
2022-12-19 21:16:26 +0000

    Fixed a bug where online status was not correctly displayed in the userinfo command because the ONLINE_STATUS cache flag was not enabled.

--- squidwardbot-misc/src/main/java/io/github/theblacksquidward/squidwardbot/misc/MiscModule.java
+++ squidwardbot-misc/src/main/java/io/github/theblacksquidward/squidwardbot/misc/MiscModule.java
@@ -2,6 +2,8 @@ package io.github.theblacksquidward.squidwardbot.misc;
 
 import io.github.theblacksquidward.squidwardbot.core.modules.ISquidwardBotModule;
 import io.github.theblacksquidward.squidwardbot.core.modules.SquidwardBotModule;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 @SquidwardBotModule
 public class MiscModule implements ISquidwardBotModule {
@@ -9,6 +11,11 @@ public class MiscModule implements ISquidwardBotModule {
     @Override
     public String getModuleIdentifier() {
         return "squidwardbot_misc";
+    }
+
+    @Override
+    public void onJDABuild(JDABuilder jdaBuilder) {
+        jdaBuilder.enableCache(CacheFlag.ONLINE_STATUS);
     }
 
 }

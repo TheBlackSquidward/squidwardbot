fe50ce973a611fab7b231db9eed5e0cd82a6cb8d
Harry Greentree <harrygreentree@icloud.com>
2023-01-19 00:20:40 +0000

    Annotated with @Serial.

--- squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/source/mirror/TrackNotFoundException.java
+++ squidwardbot-audio/src/main/java/io/github/theblacksquidward/squidwardbot/audio/source/mirror/TrackNotFoundException.java
@@ -1,7 +1,10 @@
 package io.github.theblacksquidward.squidwardbot.audio.source.mirror;
 
+import java.io.Serial;
+
 public class TrackNotFoundException extends RuntimeException {
 
+    @Serial
     private static final long serialVersionUID = 6550093849278285754L;
 
 }
